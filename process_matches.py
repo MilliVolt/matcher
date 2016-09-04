fcd4e2e3ec2c7f8c15cfc85e115b512c99930094
Viktor Roytman <vr2262@columbia.edu>
2016-09-04 17:53:01 -0400

    Add psuedocode for using subprocesses

--- process_matches.py
+++ process_matches.py
@@ -3,6 +3,7 @@
 import asyncio
 from concurrent.futures import ProcessPoolExecutor
 from functools import partial
+from multiprocessing import cpu_count
 
 from sqlalchemy.orm import sessionmaker
 
@@ -12,6 +13,8 @@ from matcher import compatibility
 
 engine = models.create_engine()
 session = sessionmaker(bind=engine, autocommit=True)()
+cpus = cpu_count()
+queue = asyncio.Queue(cpus)
 
 
 def record_match(unmatch):
@@ -53,12 +56,31 @@ async def make_matches(loop):
         await asyncio.wait([spawn(unmatch) for unmatch in unmatched])
 
 
+async def produce_tasks():
+    while True:
+        await queue.put(get_task_parameters())
+
+
+async def consume_tasks():
+    while True:
+        task_params = await queue.get()
+        proc = await asyncio.create_subprocess_exec(process_name, task_params,
+            stdout=asyncio.subprocess.PIPE)
+        data = await proc.stdout.readline()
+        line = data.decode().rstrip()
+        await proc.wait()
+        do_something_with(line)
+
+
 def main():
     """Use a process pool to carry out tasks."""
     loop = asyncio.get_event_loop()
     with ProcessPoolExecutor() as executor:
         loop.set_default_executor(executor)
         asyncio.ensure_future(make_matches(loop))
+        asyncio.ensure_future(produce_tasks())
+        for _ in range(cpus):
+             asyncio.ensure_future(consume_tasks())
         loop.run_forever()
         loop.close()
 
